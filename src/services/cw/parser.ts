f9939add5ed5ecaf96c330924cfa2d74658ace09
Kyle Husmann <kyle.husmann@gmail.com>
2023-06-02 12:38:46 -0400

    simplify with intercalate

--- src/services/cw/parser.ts
+++ src/services/cw/parser.ts
@@ -8,67 +8,50 @@ import * as R from 'fp-ts/ReadonlyRecord';
 import * as M from 'fp-ts/ReadonlyMap';
 import { pipe } from 'fp-ts/function';
 
-import { Semigroup } from 'fp-ts/Semigroup';
-
 import { match, P } from 'ts-pattern';
 
 // parser-ts
-import { parser, char, stream, string } from 'parser-ts';
+import { parser, char, stream } from 'parser-ts';
 
 import { CHAR_LOOKUP, PROSIGN_LOOKUP, LETTER_SEP, WORD_SEP, TONE_SEP, DIT, DAH } from './constants';
 
 import type { ToneSeq, Tone } from './constants';
 
-const joinToneSeqSemi = (sep: Tone): Semigroup<ToneSeq>=> ({
-  concat: (x, y) => pipe(
-    x,
-    NEA.concat([sep]),
-    NEA.concat(y),
-  )
-})
-
-const lookupProsign = (s: string): O.Option<ToneSeq> => pipe(
-    PROSIGN_LOOKUP,
-    R.lookup(s),
-);
-
-const lookupSymbol = (s: string): O.Option<ToneSeq> => pipe(
-    CHAR_LOOKUP,
-    R.lookup(s),
-);
+const parserLiftOption = <I, O>(p: parser.Parser<I, O.Option<O>>) => pipe(
+    p,
+    parser.chain((o) => pipe(
+        o,
+        O.fold(
+            () => parser.fail<I>(),
+            (o) => parser.of(o),
+        ),
+    )),
+)
 
 const prosignParser = pipe(
     parser.between(char.char("<"), char.char(">"))(char.many1(char.upper)),
-    parser.chain((s) => pipe(
-        lookupProsign(`<${s}>`),
-        O.fold(
-            () => parser.fail<string>(),
-            (cw) => parser.succeed(cw),
-        ),
-    )),
+    parser.map((s) => R.lookup(`<${s}>`)(PROSIGN_LOOKUP)),
+    parserLiftOption,
 );
 
 const charParser = pipe(
     parser.item<string>(),
-    parser.chain((s) => pipe(
-        lookupSymbol(s.toUpperCase()),
-        O.fold(
-            () => parser.fail<string>(),
-            (cw) => parser.succeed(cw),
-        )
-    )),
+    parser.map((s) => R.lookup(s.toUpperCase())(CHAR_LOOKUP)),
+    parserLiftOption,
 );
 
 const wordParser = pipe(
     parser.many1(charParser),
     parser.map(
-        NEA.foldMap(joinToneSeqSemi(LETTER_SEP))((s) => s),
+        NEA.intercalate(NEA.getSemigroup<Tone>())(NEA.of(LETTER_SEP))
     ),
 );
 
 const messageParser = pipe(
     parser.sepBy1(parser.many1(char.char(' ')), parser.either(prosignParser, () => wordParser)),
-    parser.map(NEA.foldMap(joinToneSeqSemi(WORD_SEP))((s) => s)),
+    parser.map(
+        NEA.intercalate(NEA.getSemigroup<Tone>())(NEA.of(WORD_SEP))
+    ),
 );
 
 export const parseMessage = (s: string) => pipe(
@@ -86,5 +69,5 @@ export const stringFromToneSeq = (ts: ToneSeq) => pipe(
         .with(P.union(DIT, DAH), (t) => t)
         .exhaustive()
     )),
-    A.reduce('', (acc, t) => acc + t),
+    A.reduce(S.empty, S.Monoid.concat),
 )
