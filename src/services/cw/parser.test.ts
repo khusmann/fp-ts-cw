fe75b7f49029f04c791edc12fad308b0e06291e4
Kyle Husmann <kyle.husmann@gmail.com>
2023-06-02 18:28:22 -0400

    start working on tests for parser

--- src/services/cw/parser.test.ts
+++ src/services/cw/parser.test.ts
@@ -5,20 +5,18 @@ import { run } from 'parser-ts/code-frame';
 import { pipe } from 'fp-ts/function';
 import * as E from 'fp-ts/lib/Either';
 
-describe('DitDahSeq', () => {
-    it("should work", () => {
-        pipe(
-            parseMessage("HELLO, world 73 <BT> \n"),
-            E.map((pr) => stringFromToneSeq(pr.value)),
-            E.fold((e) => e.expected.join('\n'), (s) => s),
-            console.log,
-        )
-        
-        pipe(
+describe("ToneSeq", () => {
+    it("decodes valid text with prosigns", () => {
+        const result = pipe(
             run(messageParser, "HELLo, + world 73 <BT>  \n"),
             E.map((pr) => stringFromToneSeq(pr)),
-            E.fold((e) => e, (s) => s),
-            console.log,
         )
+        expect(result).toEqualRight(".... . .-.. .-.. --- --..-- / .-.-. / .-- --- .-. .-.. -.. / --... ...-- / -...- / .-.-");
+    })
+    it("errors on invalid prosigns", () => {
+        const result = pipe(
+            parseMessage("<BR>"),
+        )
+        expect(result).toBeLeft();
     });
 });
