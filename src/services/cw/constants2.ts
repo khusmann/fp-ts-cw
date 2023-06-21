cedf60d60d211720c8887fe061acadfa48f1fbf8
Kyle Husmann <kyle.husmann@gmail.com>
2023-06-21 12:46:21 -0400

    add eslint and prettier

--- src/services/cw/constants2.ts
+++ src/services/cw/constants2.ts
@@ -1,221 +1,208 @@
-// fp-ts
-import * as S from 'fp-ts/string';
-import * as E from 'fp-ts/Either';
-import * as O from 'fp-ts/Option';
-import * as RA from 'fp-ts/ReadonlyArray';
 import * as RNA from 'fp-ts/ReadonlyNonEmptyArray';
-import * as RT from 'fp-ts/ReadonlyTuple';
 import * as RR from 'fp-ts/ReadonlyRecord';
-import * as R from 'fp-ts/Reader';
 import { pipe, flow } from 'fp-ts/function';
-
+import * as S from 'fp-ts/string';
 import { match, P } from 'ts-pattern';
 
-export const DOT = { _tag: 'dot' } as const;
+export type Dot = { readonly _tag: 'dot' };
+export type Dash = { readonly _tag: 'dash' };
+export type ToneSpace = { readonly _tag: 'tonespace' };
+export type TokenSpace = { readonly _tag: 'letterspace' };
+export type WordSpace = { readonly _tag: 'wordspace' };
 
-export type Dot = typeof DOT;
-
-export const DASH = { _tag: 'dash' } as const;
-
-export type Dash = typeof DASH;
-
-export const TONE_SPACE = { _tag: 'tonespace' } as const;
-
-export type ToneSpace = typeof TONE_SPACE;
-
-export const LETTER_SPACE = { _tag: 'letterspace' } as const;
-
-export type LetterSpace = typeof LETTER_SPACE;
-
-export const WORD_SPACE = { _tag: 'wordspace' } as const;
-
-export type WordSpace = typeof WORD_SPACE;
-
-export const Character = (str: string, children: RNA.ReadonlyNonEmptyArray<Dot | Dash | ToneSpace>) => ({
-    _tag: 'character',
-    str,
-    children,
-} as const);
-
-export type Character = ReturnType<typeof Character>;
-
-export const Prosign = (str: string, children: RNA.ReadonlyNonEmptyArray<Dot | Dash | ToneSpace>) => ({
-    _tag: 'prosign',
-    str,
-    children,
-} as const);
-
-export type Prosign = ReturnType<typeof Prosign>;
-
+export type Code = Dot | Dash | ToneSpace;
 export type Token = Character | Prosign;
 
-export const Word = (children: RNA.ReadonlyNonEmptyArray<Token | LetterSpace>) => ({
-    _tag: 'word',
-    children,
-} as const);
+export type Character = {
+  readonly _tag: 'character';
+  readonly str: string;
+  readonly children: RNA.ReadonlyNonEmptyArray<Dot | Dash | ToneSpace>;
+};
 
-export type Word = ReturnType<typeof Word>;
+export type Prosign = {
+  readonly _tag: 'prosign';
+  readonly str: string;
+  readonly children: RNA.ReadonlyNonEmptyArray<Dot | Dash | ToneSpace>;
+};
 
-export const Message = (children: RNA.ReadonlyNonEmptyArray<Word | WordSpace>) => ({
-    _tag: 'message',
-    children,
-} as const);
+export type Word = {
+  readonly _tag: 'word';
+  readonly children: RNA.ReadonlyNonEmptyArray<Token | TokenSpace>;
+};
 
-export type Message = ReturnType<typeof Message>;
+export type Message = {
+  readonly _tag: 'message';
+  readonly children: RNA.ReadonlyNonEmptyArray<Word | WordSpace>;
+};
+
+export const DOT: Dot = { _tag: 'dot' };
+export const DASH: Dash = { _tag: 'dash' };
+export const TONE_SPACE: ToneSpace = { _tag: 'tonespace' };
+export const WORD_SPACE: WordSpace = { _tag: 'wordspace' };
+export const TOKEN_SPACE: TokenSpace = { _tag: 'letterspace' };
+
+export const character = (
+  str: string,
+  children: RNA.ReadonlyNonEmptyArray<Dot | Dash | ToneSpace>
+): Character => ({
+  _tag: 'character',
+  str,
+  children,
+});
+
+export const prosign = (
+  str: string,
+  children: RNA.ReadonlyNonEmptyArray<Dot | Dash | ToneSpace>
+): Prosign => ({
+  _tag: 'prosign',
+  str,
+  children,
+});
+
+export const word = (children: RNA.ReadonlyNonEmptyArray<Token | TokenSpace>): Word => ({
+  _tag: 'word',
+  children,
+});
+
+export const message = (children: RNA.ReadonlyNonEmptyArray<Word | WordSpace>): Message => ({
+  _tag: 'message',
+  children,
+});
 
 const CW_SYMBOLS = [
-    ['A', '.-'],
-    ['B', '-...'],
-    ['C', '-.-.'],
-    ['D', '-..'],
-    ['E', '.'],
-    ['F', '..-.'],
-    ['G', '--.'],
-    ['H', '....'],
-    ['I', '..'],
-    ['J', '.---'],
-    ['K', '-.-'],
-    ['L', '.-..'],
-    ['M', '--'],
-    ['N', '-.'],
-    ['O', '---'],
-    ['P', '.--.'],
-    ['Q', '--.-'],
-    ['R', '.-.'],
-    ['S', '...'],
-    ['T', '-'],
-    ['U', '..-'],
-    ['V', '...-'],
-    ['W', '.--'],
-    ['X', '-..-'],
-    ['Y', '-.--'],
-    ['Z', '--..'],
-    
-    ['0', '-----'],
-    ['1', '.----'],
-    ['2', '..---'],
-    ['3', '...--'],
-    ['4', '....-'],
-    ['5', '.....'],
-    ['6', '-....'],
-    ['7', '--...'],
-    ['8', '---..'],
-    ['9', '----.'],
+  ['A', '.-'],
+  ['B', '-...'],
+  ['C', '-.-.'],
+  ['D', '-..'],
+  ['E', '.'],
+  ['F', '..-.'],
+  ['G', '--.'],
+  ['H', '....'],
+  ['I', '..'],
+  ['J', '.---'],
+  ['K', '-.-'],
+  ['L', '.-..'],
+  ['M', '--'],
+  ['N', '-.'],
+  ['O', '---'],
+  ['P', '.--.'],
+  ['Q', '--.-'],
+  ['R', '.-.'],
+  ['S', '...'],
+  ['T', '-'],
+  ['U', '..-'],
+  ['V', '...-'],
+  ['W', '.--'],
+  ['X', '-..-'],
+  ['Y', '-.--'],
+  ['Z', '--..'],
 
-    ['.', '.-.-.-'],
-    [',', '--..--'],
-    ['?', '..--..'],
-    ['\'', '.----.'],
-    ['\n', '.-.-'],
-    ['!', '-.-.--'],
-    ['/', '-..-.'],
-    ['(', '-.--.'],
-    [')', '-.--.-'],
-    ['&', '.-...'],
-    [':', '---...'],
-    [';', '-.-.-.'],
-    ['=', '-...-'],
-    ['+', '.-.-.'],
-    ['-', '-....-'],
-    ['_', '..--.-'],
-    ['"', '.-..-.'],
-    ['$', '...-..-'],
-    ['@', '.--.-.'],
-    ['#', '...-.-'],
+  ['0', '-----'],
+  ['1', '.----'],
+  ['2', '..---'],
+  ['3', '...--'],
+  ['4', '....-'],
+  ['5', '.....'],
+  ['6', '-....'],
+  ['7', '--...'],
+  ['8', '---..'],
+  ['9', '----.'],
 
-    ['AA', '.-.-'],
-    ['AR', '.-.-.'],
-    ['AS', '.-...'],
-    ['BT', '-...-'],
-    ['BK', '-...-.-'],
-    ['CL', '-.-..-..'],
-    ['CT', '-.-.-'],
-    ['DO', '-..---'],
-    ['KA', '-.-.-'],
-    ['KN', '-.--.'],
-    ['SK', '...-.-'],
-    ['VA', '...-.-'],
-    ['VE', '...-.'],
-    ['SOS', '...---...'],
+  ['.', '.-.-.-'],
+  [',', '--..--'],
+  ['?', '..--..'],
+  ["'", '.----.'],
+  ['\n', '.-.-'],
+  ['!', '-.-.--'],
+  ['/', '-..-.'],
+  ['(', '-.--.'],
+  [')', '-.--.-'],
+  ['&', '.-...'],
+  [':', '---...'],
+  [';', '-.-.-.'],
+  ['=', '-...-'],
+  ['+', '.-.-.'],
+  ['-', '-....-'],
+  ['_', '..--.-'],
+  ['"', '.-..-.'],
+  ['$', '...-..-'],
+  ['@', '.--.-.'],
+  ['#', '...-.-'],
+
+  ['AA', '.-.-'],
+  ['AR', '.-.-.'],
+  ['AS', '.-...'],
+  ['BT', '-...-'],
+  ['BK', '-...-.-'],
+  ['CL', '-.-..-..'],
+  ['CT', '-.-.-'],
+  ['DO', '-..---'],
+  ['KA', '-.-.-'],
+  ['KN', '-.--.'],
+  ['SK', '...-.-'],
+  ['VA', '...-.-'],
+  ['VE', '...-.'],
+  ['SOS', '...---...'],
 ] as const;
 
 const _pulsesFromCode = flow(
-    S.split(''),
-    RNA.map((c) => c === '.' ? DOT : DASH),
-    RNA.intersperse<Dot | Dash | ToneSpace>(TONE_SPACE),
+  S.split(''),
+  RNA.map((c) => (c === '.' ? DOT : DASH)),
+  RNA.intersperse<Dot | Dash | ToneSpace>(TONE_SPACE)
 );
 
-const _tokenFromCode = (str: string, code: string): Token => (
-    str.length === 1 ? Character(str, _pulsesFromCode(code)) : Prosign(str, _pulsesFromCode(code))
-);
+const _tokenFromCode = (str: string, code: string): Token =>
+  str.length === 1 ? character(str, _pulsesFromCode(code)) : prosign(str, _pulsesFromCode(code));
 
 export const CW_TOKEN_LOOKUP = pipe(
-    CW_SYMBOLS,
-    RNA.map(([str, code]) => [str, _tokenFromCode(str, code)] as const),
-    RR.fromEntries,
+  CW_SYMBOLS,
+  RNA.map(([str, code]: (typeof CW_SYMBOLS)[number]) => [str, _tokenFromCode(str, code)] as const),
+  RR.fromEntries
 );
 
 export const CW_CODE_LOOKUP = pipe(
-    CW_SYMBOLS,
-    RNA.map(([str, code]) => [code, _tokenFromCode(str, code)] as const),
-    RR.fromEntries,
+  CW_SYMBOLS,
+  RNA.map(([str, code]: (typeof CW_SYMBOLS)[number]) => [code, _tokenFromCode(str, code)] as const),
+  RR.fromEntries
 );
 
-export const stringifyTokens = (m: Message | Prosign | Character | Word | WordSpace | LetterSpace | Prosign) => (
-    match(m)
-    .with(P.union(
-        { _tag: 'message' },
-        { _tag: 'word' },
-    ), ({ children }) => pipe(
-        children,
-        RNA.map(stringifyTokens),
-        RNA.concatAll(S.Semigroup),
-    ))
+export const stringifyTokens = (m: Message | Word | WordSpace | TokenSpace | Token): string =>
+  match(m)
+    .with(P.union({ _tag: 'message' }, { _tag: 'word' }), ({ children }) =>
+      pipe(children, RNA.map(stringifyTokens), RNA.concatAll(S.Semigroup))
+    )
     .with({ _tag: 'prosign' }, ({ str }) => `<${str}>`)
     .with({ _tag: 'character' }, ({ str }) => str)
     .with({ _tag: 'wordspace' }, () => ' ')
     .with({ _tag: 'letterspace' }, () => '')
-    .exhaustive()
-)
+    .exhaustive();
 
-export const stringifyCode = (m: Message | Prosign | Character | Word | WordSpace | LetterSpace | Prosign | Dot | Dash | ToneSpace) => (
-    match(m)
-    .with(P.union(
-        { _tag: 'message' },
-        { _tag: 'word' },
-        { _tag: 'prosign' },
-        { _tag: 'character' },
-    ), ({ children }) => pipe(
-        children,
-        RNA.map(stringifyCode),
-        RNA.concatAll(S.Semigroup),
-    ))
+export const stringifyCode = (
+  m: Message | Token | Word | WordSpace | TokenSpace | Dot | Dash | ToneSpace
+): string =>
+  match(m)
+    .with(
+      P.union({ _tag: 'message' }, { _tag: 'word' }, { _tag: 'prosign' }, { _tag: 'character' }),
+      ({ children }) => pipe(children, RNA.map(stringifyCode), RNA.concatAll(S.Semigroup))
+    )
     .with({ _tag: 'wordspace' }, () => ' / ')
     .with({ _tag: 'letterspace' }, () => ' ')
     .with({ _tag: 'dot' }, () => '.')
     .with({ _tag: 'dash' }, () => '-')
     .with({ _tag: 'tonespace' }, () => '')
-    .exhaustive()
-);
+    .exhaustive();
 
-export const stringifyPulses = (m: Message | Prosign | Character | Word | WordSpace | LetterSpace | Prosign | Dot | Dash | ToneSpace) => (
-    match(m)
-    .with(P.union(
-        { _tag: 'message' },
-        { _tag: 'word' },
-        { _tag: 'prosign' },
-        { _tag: 'character' },
-    ), ({ children }) => pipe(
-        children,
-        RNA.map(stringifyPulses),
-        RNA.concatAll(S.Semigroup),
-    ))
+export const stringifyPulses = (
+  m: Message | Word | WordSpace | TokenSpace | Token | Dot | Dash | ToneSpace
+): string =>
+  match(m)
+    .with(
+      P.union({ _tag: 'message' }, { _tag: 'word' }, { _tag: 'prosign' }, { _tag: 'character' }),
+      ({ children }) => pipe(children, RNA.map(stringifyPulses), RNA.concatAll(S.Semigroup))
+    )
     .with({ _tag: 'wordspace' }, () => ' ')
     .with({ _tag: 'letterspace' }, () => '/')
     .with({ _tag: 'dot' }, () => '.')
     .with({ _tag: 'dash' }, () => '-')
     .with({ _tag: 'tonespace' }, () => '|')
-    .exhaustive()
-);
-
-
+    .exhaustive();
--- src/services/cw/parser2.ts
+++ src/services/cw/parser2.ts
@@ -1,87 +1,87 @@
-// fp-ts
-import * as S from 'fp-ts/string';
-import * as E from 'fp-ts/Either';
 import * as O from 'fp-ts/Option';
-import * as RA from 'fp-ts/ReadonlyArray';
 import * as RNA from 'fp-ts/ReadonlyNonEmptyArray';
 import * as RR from 'fp-ts/ReadonlyRecord';
-import * as R from 'fp-ts/Reader';
 import { pipe } from 'fp-ts/function';
+import { parser, char, string } from 'parser-ts';
 
-import { match, P } from 'ts-pattern';
+import {
+  CW_TOKEN_LOOKUP,
+  WORD_SPACE,
+  word,
+  message,
+  TOKEN_SPACE,
+  CW_CODE_LOOKUP,
+} from './constants2';
+import type { Word, WordSpace, TokenSpace, Token } from './constants2';
 
-// parser-ts
-import { parser, char, stream, string, } from 'parser-ts';
-
-import { CW_TOKEN_LOOKUP, WORD_SPACE, Word, Message, LETTER_SPACE, CW_CODE_LOOKUP } from './constants2';
-
-import type { Character, Prosign, WordSpace, LetterSpace, Dot, Dash, ToneSpace, Token } from './constants2';
-
-const parserFromOption = <I>(): (<O>(o: O.Option<O>) => parser.Parser<I, O>) => (
-    O.fold(
-        () => parser.fail<I>(),
-        (o) => parser.succeed(o),
-    )
-);
+const parserFromOption = <I>(): (<O>(o: O.Option<O>) => parser.Parser<I, O>) =>
+  O.fold(
+    () => parser.fail<I>(),
+    (o) => parser.succeed(o)
+  );
 
 const parseProsignText = pipe(
-    parser.between(char.char("<"), char.char(">"))(char.many1(char.upper)),
-    parser.map((s) => RR.lookup(s)(CW_TOKEN_LOOKUP)),
-    parser.chain(parserFromOption()),
+  parser.between(char.char('<'), char.char('>'))(char.many1(char.upper)),
+  parser.map((s) => RR.lookup(s)(CW_TOKEN_LOOKUP)),
+  parser.chain(parserFromOption())
 );
 
 const parseCharacterText = pipe(
-    parser.item<string>(),
-    parser.map((s) => RR.lookup(s.toUpperCase())(CW_TOKEN_LOOKUP)),
-    parser.chain(parserFromOption()),
-)
+  parser.item<string>(),
+  parser.map((s) => RR.lookup(s.toUpperCase())(CW_TOKEN_LOOKUP)),
+  parser.chain(parserFromOption())
+);
 
 const parseWordSpaceText = pipe(
-    char.char(' '),
-    parser.map(() => WORD_SPACE),
+  char.char(' '),
+  parser.map(() => WORD_SPACE)
 );
 
 const parseTokenText = parser.either<string, Token>(parseProsignText, () => parseCharacterText);
 
 const parseWordText = pipe(
-    parser.many1(parseTokenText),
-    parser.map(RNA.intersperse<Token | LetterSpace>(LETTER_SPACE)),
-    parser.map(Word),
+  parser.many1(parseTokenText),
+  parser.map(RNA.intersperse<Token | TokenSpace>(TOKEN_SPACE)),
+  parser.map(word)
 );
 
-export const parseMessageText: parser.Parser<string, Message> = parser.expected(
-    pipe(
-        parser.many1Till(parser.either<string, Word | WordSpace>(parseWordText, () => parseWordSpaceText), parser.eof()),
-        parser.map(Message),
+export const parseMessageText = parser.expected(
+  pipe(
+    parser.many1Till(
+      parser.either<string, Word | WordSpace>(parseWordText, () => parseWordSpaceText),
+      parser.eof()
     ),
-    "valid character or prosign"
+    parser.map(message)
+  ),
+  'valid character or prosign'
 );
 
 const parseWordSpaceCode = pipe(
-    char.char('/'),
-    parser.chain(() => parser.succeed(WORD_SPACE)),
+  char.char('/'),
+  parser.chain(() => parser.succeed(WORD_SPACE))
 );
 
 const parseTokenCode = pipe(
-    string.many1(parser.either(char.char('.'), () => char.char('-'))),
-    parser.map((s) => RR.lookup(s)(CW_CODE_LOOKUP)),
-    parser.chain(parserFromOption()),
+  string.many1(parser.either(char.char('.'), () => char.char('-'))),
+  parser.map((s) => RR.lookup(s)(CW_CODE_LOOKUP)),
+  parser.chain(parserFromOption())
 );
 
 const parseWordCode = pipe(
-    parser.sepBy1(parser.many(char.space), parseTokenCode),
-    parser.map(RNA.intersperse<Token | LetterSpace>(LETTER_SPACE)),
-    parser.map(Word),
+  parser.sepBy1(parser.many(char.space), parseTokenCode),
+  parser.map(RNA.intersperse<Token | TokenSpace>(TOKEN_SPACE)),
+  parser.map(word)
 );
 
 export const parseMessageCode = parser.expected(
-    pipe(
-        parser.many1Till(
-            parser.surroundedBy(parser.many(char.space))(
-                parser.either<string, Word | WordSpace>(parseWordCode, () => parseWordSpaceCode)
-            ), parser.eof(),
-        ),
-        parser.map(Message),
+  pipe(
+    parser.many1Till(
+      parser.surroundedBy(parser.many(char.space))(
+        parser.either<string, Word | WordSpace>(parseWordCode, () => parseWordSpaceCode)
+      ),
+      parser.eof()
     ),
-    "valid character or prosign",
+    parser.map(message)
+  ),
+  'valid character or prosign'
 );
